eb06c68bfbf0f1cb8502eebc9ed79d11a7974881
Warsgame616 <menardcharles616@users.noreply.github.com>
2026-09-21 14:17:17 +0000

    Hero: boutons toujours visibles - Inscription/Connexion pour les visiteurs, Mon espace/Deconnexion pour les connectes

--- index.php
+++ index.php
@@ -19,6 +19,9 @@ require_once __DIR__ . '/includes/header.php';
         <?php if (!is_logged()): ?>
           <a href="inscription.php" class="btn btn-outline btn-lg">Inscription</a>
           <a href="connexion.php" class="btn btn-outline btn-lg">Connexion</a>
+        <?php else: ?>
+          <a href="<?= dashboard_url() ?>" class="btn btn-outline btn-lg">Mon espace</a>
+          <a href="deconnexion.php" class="btn btn-outline btn-lg">Déconnexion</a>
         <?php endif; ?>
         <a href="entreprises.php" class="btn btn-ghost btn-lg">Comment ça marche</a>
       </div>
